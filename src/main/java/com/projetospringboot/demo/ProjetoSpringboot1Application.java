7ea6eaa7003482fd39109ae4de698a531c0e98bd
Luís Felipe <felipe.drs01@gmail.com>
2022-05-11 14:59:21 -0300

    project created

---
+++ src/main/java/com/projetospringboot/demo/ProjetoSpringboot1Application.java
@@ -0,0 +1,13 @@
+package com.projetospringboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringboot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringboot1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetospringboot/demo/ProjetoSpringboot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetospringboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringboot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
